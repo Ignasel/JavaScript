ea8c81172c9cb26f271953a44f0980a5e87f77d8
Your Name <you@example.com>
2019-11-27 23:27:05 +0200

    Su Manto pagalba

--- AjaxUzd.js
+++ AjaxUzd.js
@@ -1,28 +1,43 @@
-// const raudonas =
-//     const zalias =
-const mokiniai = document.querySelector("ul")
 
 
-var xhr = new XMLHttpRequest();
-
+let xhr = new XMLHttpRequest();
 xhr.onreadystatechange = function() {
     if (xhr.readyState === 4) {
-        var duomenys = JSON.parse(xhr.responseText);
-    }
-    for (rows of duomenys) {
-        for (info in rows) {
-                mokiniai.textContent = info
-                }
-                // if (info == "lokacija"){
-                // if (rows[info] == true){
-                //     mokiniai.innerHTML =
-                //     }
-                //  }
+        let duomenys = JSON.parse(xhr.responseText);
+        for (let rows of duomenys) {
+            const mokiniai = document.querySelector(".turinys")
+            const color = document.createElement("span")
+            const mokinys = document.createElement("h5")
+
+            mokinys.textContent = rows.vardas;
+
+            mokinys.classList.add("badge", "badge-pill");
+            mokinys.classList.add("badge-success");
+            mokinys.textContent = 'Yra';
+
+            if (!mokinys.location) {
+                mokinys.classList.remove("badge-success");
+                mokinys.classList.add("badge-danger");
+                mokinys.textContent = 'Nėra';
+            }
+
+            mokiniai.appendChild(color);
+            mokiniai.appendChild(mokinys);
+
+
+
+
+
+
+
+        }
+            // if (info == "lokacija"){
+            // if (rows[info] == true){
+            //     mokiniai.innerHTML =
+            //     }
+            //  }
 
         }
     }
-
-
 xhr.open('GET', 'duomenys.json');
-
 xhr.send();
